028e09e35eb2e977b022b2dc58786f2b6afeb909
marbinowosito <tujamoxd123@gmail.com>
2022-03-03 00:18:45 -0600

    responsive part one

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 const bars = document.getElementById('bars');
-const items = document.getElementById('nav-items-hidde');
+const items = document.querySelector('.navbar-items');
 
 bars.addEventListener('click', () => {
   items.classList.toggle('d-none');
+
+  if (bars.classList.contains('fa-bars')) {
+    bars.classList.remove('fa-bars');
+    bars.classList.add('fa-xmark');
+  } else {
+    bars.classList.remove('fa-xmark');
+    bars.classList.add('fa-bars');
+  }
 })
